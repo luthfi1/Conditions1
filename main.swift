0baa5af74c6f24763b85429bf1596b652d209149
Muhammad Luthfi Farizqi Luthfi <luthfifarizqi23@gmail.com>
2020-10-21 03:22:19 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,40 @@
+//Conditions
+/*
+let first = 11
+let second = 16
+
+if first <= second  {
+    print("Blackjack")
+}else{
+  print("No Blackjack")
+}
+*/
+let first = 11
+let second = 16
+
+if first >= second  {
+    print("Blackjack")
+}else{
+  print("No Blackjack")
+}
+
+/*
+let firstCard = 11
+let secondCard = 10
+
+if firstCard + secondCard == 21 {
+    print("LUTHFI FARIZQI")
+}
+if firstCard + secondCard == 21 {
+    print("RPL")
+} else {
+    print("Regular cards")
+}
+if firstCard + secondCard == 2 {
+    print("Aces – lucky!")
+} else if firstCard + secondCard == 21 {
+    print("A")
+} else {
+    print("Regular cards")
+}
+*/
